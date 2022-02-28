594a07358a2334bbe1f18220aaa8a4ba9222abf7
Adam Biedka Whitton <adambb914@gmail.com>
2022-02-28 17:14:30 +0100

    controllers added

--- app/models/subtopic.rb
+++ app/models/subtopic.rb
@@ -1,4 +1,15 @@
 class Subtopic < ApplicationRecord
-  belongs_to :subscription
+  NEWS = ["World", "Regional", "Local", "Environental", "Politics"]
+  SPORT = ["Football", "Rugby", "Tennis", "Golf", "Basketball", "Baseball", "American Football", "Handball", "Ice Hockey"]
+  CULTURE = ["Art", "Architecture", "History", "Design", "Theatre", "Movies", "Literature", "Events"]
+  FINANCE = ["Stock Exchange", "Cryptos", "Mortgages"]
+  TRAVELLING = ["Hotels", "Flights", "Deals", "Events"]
+  REALESTATE = ["Rental Market", "Buying Market", "Development"]
+  TECH = ["Coding", "Gadgets", "Silicon Valley", "Future"]
+  PETS = ["Dogs", "Cats", "Birds", "Elephants"]
+  has_many :subscriptions
   belongs_to :topic
+
+  validates :title, presence: true
+  validates :title, inclusion: { in: [NEWS, SPORT, CULTURE, FINANCE, TRAVELLING, REALESTATE, TECH, PETS] }
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,7 @@
 class Topic < ApplicationRecord
+  TITLES = ["News", "Weather", "Sport", "Culture", "Finance", "Travelling", "Real Estate", "Tech", "Pets"]
+  has_many :subtopics
+
+  validates :title, presence: true
+  validates :title, inclusion: { in: TITLES }
 end
