64d4c1dea2f26c6e1511ce3cc4d4f67cee5c9f02
Adam Biedka Whitton <adambb914@gmail.com>
2022-03-01 14:44:37 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :topics, only: [:index, :show] do
+    resources :subtopics, only: [:index, :show, :create]
+  end
+  resources :tiles, only: [:index, :show, :destroy]
 end
