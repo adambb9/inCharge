ee9eb46d8d4d0fe1c27b27cd5648b3a9170b1ef9
ErnaHreinsdottir <erna.hreins@gmail.com>
2022-03-01 12:36:12 +0100

    topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,9 @@
+class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Topic.create(title: "BREAKING", summary: "Soviet Union during the Cold War", content: "Some men come into conflict with the Soviet Union during the Cold War in the 1980s", photo: "https://www.fundaciontelefonica.com/wp-content/uploads/2018/10/expo-national-geographic.jpg")
