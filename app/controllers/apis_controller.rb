d8af2f17a9d23ccd25d1b4a81ad4da256a7a4808
Adam Biedka Whitton <adambb914@gmail.com>
2022-03-08 11:41:18 +0100

    one thing

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -6,6 +6,7 @@ class ApisController < ApplicationController
   def show
     url = build_api_query('top-headlines?q=trump')
     @response = parse_query(url)
+    create_tile
   end
 
   def new
@@ -17,24 +18,25 @@ class ApisController < ApplicationController
   end
 
   def build_api_query(query)
-    url = "https://newsapi.org/v2/#{query}&apiKey=8d87341021534a57b58acbaf56e2aaaf"
+    url = "https://newsapi.org/v2/#{query}&pageSize=1&apiKey=8d87341021534a57b58acbaf56e2aaaf"
   end
 
   def parse_query(url)
     response = URI.open(url).read
-    parsed_response = JSON.parse(response)[articles]
+    parsed_response = JSON.parse(response)["articles"][0]
   end
 
   def create_tile
     url = build_api_query('top-headlines?q=trump')
     @response = parse_query(url)
     @tile = Tile.new
-    @tile.title = @response.title
-    @tile.summary = @repsonse.description
-    @tile.content = @response.content
-    @tile.picture_url = @reponse.urlToImage
-    @tile.source = @response.author
-    @tile.url = @response.url
+    @tile.title = @response["title"]
+    @tile.summary = @response["description"]
+    @tile.content = @response["content"]
+    @tile.picture_url = @response["urlToImage"]
+    @tile.source = @response["source"]["name"]
+    @tile.author = @response["author"]
+    @tile.url = @response["url"]
     @tile.subtopic_id = 1
     if @tile.save!
       redirect_to tile_path(@tile)
