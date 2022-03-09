81890a611a4204797491f19e048b8e24ab62f0ba
Adam Biedka Whitton <adambb914@gmail.com>
2022-03-09 12:49:29 +0100

    create workign

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  LANG_CATEGORIES = ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "se", "ud", "zh"]
-  COUNTRY_CATEGORIES = ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "se", "ud", "zh"]
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
