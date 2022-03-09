74b154821b0bb088bbffa229d1f1f88825ab7030
Adam Biedka Whitton <adambb914@gmail.com>
2022-03-09 08:54:55 +0100

    complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  LANG_CATEGORIES = ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "se", "ud", "zh"]
+  COUNTRY_CATEGORIES = ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "se", "ud", "zh"]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 
   has_one_attached :photo
 
